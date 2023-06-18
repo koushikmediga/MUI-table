4299ca7aa403e0841d86ea726503494d782e589e
Mediga Sai Koushik <medigasaikoushik@gmail.com>
2023-06-18 18:26:27 +0000

    modified typescript version

--- src/EnvTest.js
+++ src/EnvTest.js
@@ -7,7 +7,11 @@ const secretsManager = new AWS.SecretsManager();
 
 export async function loadSecrets(): Promise<void> {
   try {
-    const secretName = process.env.REACT_APP_SECRET_NAME;
+    const secretName: string | undefined = process.env.REACT_APP_SECRET_NAME;
+    if (!secretName) {
+      throw new Error('Secret name is not defined.');
+    }
+
     const data = await secretsManager.getSecretValue({ SecretId: secretName }).promise();
     if ('SecretString' in data) {
       const secrets = JSON.parse(data.SecretString);
